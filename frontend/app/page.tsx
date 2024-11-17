8a01f2b459793ce1cd0ddbcc6772a1fb031ad9b7
Kartik deshmukh <kartikdd90@gmail.com>
2024-11-17 20:50:21 +0530

    Home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,13 +6,10 @@ export default function Home() {
   return (
     <BackgroundBeamsWithCollision>
       <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
-        What&apos;s cooler than Beams?{" "}
+        I&apos;m too lazy to build a home page, so please just go to login{" "}
         <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-          <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-            <span className="">Exploding beams.</span>
-          </div>
           <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
-            <span className="">Exploding beams.</span>
+            <a href="/auth/login">Click here</a>
           </div>
         </div>
       </h2>
